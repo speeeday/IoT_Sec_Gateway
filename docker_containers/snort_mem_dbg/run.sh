8c74c1a9984964cbb16ea8f6550292d5c41a3d8f
Sanjay C <sanjayc@andrew.cmu.edu#>
2019-01-21 14:00:49 +0000

    updated repo

---
+++ docker_containers/snort_mem_dbg/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while true; do
+    grep -q '^1$' "/sys/class/net/eth0/carrier" &&
+	grep -q '^1$' "/sys/class/net/eth1/carrier" &&
+	break
+
+    sleep 1
+
+done
+
+# exec /usr/local/bin/snort "$@"
+exec /bin/bash
